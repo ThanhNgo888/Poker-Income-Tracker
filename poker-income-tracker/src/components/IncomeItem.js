c61c49ef7d6edb5e63b79de9a2198e08c5992bed
Thanh Ngo <thanhngo26@hotmail.com>
2022-12-26 15:01:23 -0800

    updated

--- poker-income-tracker/src/components/IncomeItem.js
+++ poker-income-tracker/src/components/IncomeItem.js
@@ -13,8 +13,10 @@ function IncomeItem( { income, index, removeIncome}) {
   return (
     <div className='income-item'>
         <button className='remove-item' onClick={() =>
-        removeHandle(i)}>x</button>
-      
+        removeHandle(index)}>x</button>
+        <div className='desc'>{income.desc}</div>
+        <div className='price'>${income.price}</div>
+        <div className='date'>{month + "/" + day + "/" + year}</div>
     </div>
   )
 }
