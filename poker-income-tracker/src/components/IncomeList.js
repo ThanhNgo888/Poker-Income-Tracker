9b7b21f48ada20d0815cc629c2b9cd122653a62a
Thanh Ngo <thanhngo26@hotmail.com>
2022-12-26 15:16:34 -0800

    bug on date

--- poker-income-tracker/src/components/IncomeList.js
+++ poker-income-tracker/src/components/IncomeList.js
@@ -1,21 +1,27 @@
-//rfce
 import React from 'react';
 import IncomeItem from './IncomeItem';
 
 function IncomeList({ income, setIncome }) {
-    const removeIncome = i => {
-        let temp = income.filter((v, index) => index != i);
-        setIncome(temp);
-    }
+
+  const removeIncome = i => {
+    let temp = income.filter((v, index) => index != i);
+    setIncome(temp);
+  }
+
+  const sortByDate = (a, b) => {
+    return a.date - b.date;
+  }
+
   return (
-    <div className='income-list'>
+    <div className="income-list">
       {
-        income.sort(sortByDate). map((value, index) => (
-            <IncomeItem 
+        income.sort(sortByDate).map((value, index) => (
+          <IncomeItem 
             key={index} 
             income={value} 
-            index={index}
-            removeIncome={removeIncome} />
+            index={index} 
+            removeIncome={removeIncome}
+          />
         ))
       }
     </div>
