fecdc795eed067c6e13156eb610a93e57d783bab
Thanh Ngo <thanhngo26@hotmail.com>
2022-12-26 14:45:51 -0800

    update income using useEffect

--- poker-income-tracker/src/App.js
+++ poker-income-tracker/src/App.js
@@ -2,18 +2,26 @@ import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import IncomeForm from './components/IncomeForm';
 import './App.css';
-
+//=========================================================
 function App() {
   const [income, setIncome] = useState([]);
   const [totalIncome, setTotalIncome] = useState(0);
-
+//=========================================================
+//update useEffect when income changes
+useEffect (() => {
+  let temp =0;
+  for(let i =0; i < income.length; i++){
+    temp += parseInt(income[i].price);
+  }
+  setTotalIncome(temp);
+},[income]);
+//=========================================================
   return (
     <div className="App">
       <Header totalIncome = {totalIncome}/>
       <IncomeForm income={income} setIncome={setIncome}/>
-      { (income.length > 0) ? income[0].desc : ''}
     </div>
   );
 }
-
+//=========================================================
 export default App;
